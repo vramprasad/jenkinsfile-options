cb2f016ee6ef9354da06eadb3302385079515447
Ramprasad V <vrprasad@gmail.com>
2023-07-29 08:30:30 +0530

    Added sonar

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -11,3 +11,8 @@ dummyvar='DOCAPPVERSION'
 release_ver=${BUILD_NUMBER}
 sed "s/$dummyvar/$release_ver/g" pom.xml > pom1.xml
 mv pom1.xml pom.xml
+
+#mvn sonar:sonar \
+# -Dsonar.projectKey=docapp01 \
+#  -Dsonar.host.url=http://localhost:9000 \
+#  -Dsonar.login=f9c54e719f23870eca50e027693bddc871ee41b8
